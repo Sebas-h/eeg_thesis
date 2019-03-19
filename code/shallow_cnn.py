f5821ba765b925a7c066d56d4e36d8c37ffcd5f1
Sebas <sebashigler@gmail.com>
2019-03-19 16:50:20 +0000

    changes

--- code/shallow_cnn.py
+++ code/shallow_cnn.py
@@ -33,6 +33,31 @@ from braindecode.experiments.monitors import compute_preds_per_trial_from_crops
 ####################################################################################
 ####################################################################################
 
+import os.path
+import time, datetime
+from collections import OrderedDict
+import sys
+
+from braindecode.models.deep4 import Deep4Net
+from braindecode.models.util import to_dense_prediction_model
+from braindecode.datasets.bcic_iv_2a import BCICompetition4Set2A
+from braindecode.experiments.experiment import Experiment
+from braindecode.experiments.monitors import LossMonitor, MisclassMonitor, \
+    RuntimeMonitor, CroppedTrialMisclassMonitor
+from braindecode.experiments.stopcriteria import MaxEpochs, NoDecrease, Or
+from braindecode.datautil.iterators import CropsFromTrialsIterator
+from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
+from braindecode.datautil.splitters import split_into_two_sets
+from braindecode.torch_ext.constraints import MaxNormDefaultConstraint
+from braindecode.torch_ext.util import set_random_seeds, np_to_var
+from braindecode.mne_ext.signalproc import mne_apply
+from braindecode.datautil.signalproc import (bandpass_cnt,
+                                             exponential_running_standardize)
+from braindecode.datautil.trial_segment import create_signal_target_from_raw_mne
+
+####################################################################################
+####################################################################################
+
 # path_to_data = "/Users/sebas/code/thesis/data/bcic_iv_2a_all_9_subjects.pickle"
 path_to_data = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data')) + \
                "/bcic_iv_2a_all_9_subjects.pickle"
@@ -50,8 +75,80 @@ data = data[subject_id - 1]
 
 # Split data into train, validation and test sets:
 # train_set, valid_set, test_set = splitters.split_into_train_valid_test(data, 4, 0)
-train_valid_set, test_set = splitters.split_into_two_sets(data, first_set_fraction=0.5)
-train_set, valid_set = splitters.split_into_two_sets(train_valid_set, first_set_fraction=0.8)
+# train_valid_set, test_set = splitters.split_into_two_sets(data, first_set_fraction=0.5)
+# train_set, valid_set = splitters.split_into_two_sets(train_valid_set, first_set_fraction=0.8)
+
+####################################################################################
+####################################################################################
+
+
+data_folder = '/Users/sebas/code/_eeg_data/BCICIV_2a_gdf/'
+subject_id = 1 # 1-9
+low_cut_hz = 4 # 0 or 4
+
+
+ival = [-500, 4000]
+input_time_length = 1000
+batch_size = 60
+high_cut_hz = 38
+factor_new = 1e-3
+init_block_size = 1000
+valid_set_fraction = 0.2
+
+train_filename = 'A{:02d}T.gdf'.format(subject_id)
+test_filename = 'A{:02d}E.gdf'.format(subject_id)
+train_filepath = os.path.join(data_folder, train_filename)
+test_filepath = os.path.join(data_folder, test_filename)
+train_label_filepath = train_filepath.replace('.gdf', '.mat')
+test_label_filepath = test_filepath.replace('.gdf', '.mat')
+
+train_loader = BCICompetition4Set2A(
+    train_filepath, labels_filename=train_label_filepath)
+test_loader = BCICompetition4Set2A(
+    test_filepath, labels_filename=test_label_filepath)
+train_cnt = train_loader.load()
+test_cnt = test_loader.load()
+
+# Preprocessing
+
+train_cnt = train_cnt.drop_channels(['STI 014', 'EOG-left',
+                                        'EOG-central', 'EOG-right'])
+assert len(train_cnt.ch_names) == 22
+# lets convert to millvolt for numerical stability of next operations
+train_cnt = mne_apply(lambda a: a * 1e6, train_cnt)
+train_cnt = mne_apply(
+    lambda a: bandpass_cnt(a, low_cut_hz, high_cut_hz, train_cnt.info['sfreq'],
+                            filt_order=3,
+                            axis=1), train_cnt)
+train_cnt = mne_apply(
+    lambda a: exponential_running_standardize(a.T, factor_new=factor_new,
+                                                init_block_size=init_block_size,
+                                                eps=1e-4).T,
+    train_cnt)
+
+test_cnt = test_cnt.drop_channels(['STI 014', 'EOG-left',
+                                    'EOG-central', 'EOG-right'])
+assert len(test_cnt.ch_names) == 22
+test_cnt = mne_apply(lambda a: a * 1e6, test_cnt)
+test_cnt = mne_apply(
+    lambda a: bandpass_cnt(a, low_cut_hz, high_cut_hz, test_cnt.info['sfreq'],
+                            filt_order=3,
+                            axis=1), test_cnt)
+test_cnt = mne_apply(
+    lambda a: exponential_running_standardize(a.T, factor_new=factor_new,
+                                                init_block_size=init_block_size,
+                                                eps=1e-4).T,
+    test_cnt)
+
+marker_def = OrderedDict([('Left Hand', [1]), ('Right Hand', [2],),
+                            ('Foot', [3]), ('Tongue', [4])])
+
+train_set = create_signal_target_from_raw_mne(train_cnt, marker_def, ival)
+test_set = create_signal_target_from_raw_mne(test_cnt, marker_def, ival)
+
+train_set, valid_set = split_into_two_sets(
+    train_set, first_set_fraction=1-valid_set_fraction) 
+
 
 ####################################################################################
 ####################################################################################
