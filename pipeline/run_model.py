4abbc28541c6de11e970ad8a3ace1ea9d1dae407
Sebas <sebashigler@gmail.com>
2019-04-10 21:06:11 +0100

    adds

--- pipeline/run_model.py
+++ pipeline/run_model.py
@@ -17,7 +17,7 @@ class RunModel:
         # config
         ############################################
         # 'shallow' or 'deep' or 'eegnet'
-        model_name = 'shallow'
+        model_name = 'deep'
         # cropped or trialwise training
         cropped = False
         # cross validation yes or no
