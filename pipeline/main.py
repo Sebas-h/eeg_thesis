d9f45a51620c5830cf577b6bc57d2280133b846d
Sebas <sebashigler@gmail.com>
2019-04-10 20:02:53 +0100

    adds

--- pipeline/main.py
+++ pipeline/main.py
@@ -38,11 +38,11 @@ data_subject_4 = dataset_bcic_iv_2a[3]
 data_subjects_allbut1 = data_splitters.concatenate_sets(dataset_bcic_iv_2a[1:])
 
 # Split data into train, valid, test
-train_set, valid_set, test_set = data_splitters.split_into_train_valid_test(data_subject_1, n_folds, 0)
+# train_set, valid_set, test_set = data_splitters.split_into_train_valid_test(data_subject_1, n_folds, 0)
 
 # TL without retraining:
-# train_set, valid_set = data_splitters.split_into_train_test(data_subjects_allbut1, 3, 0)
-# _, _, test_set = data_splitters.split_into_train_valid_test(data_subject_1, n_folds, 0)
+train_set, valid_set = data_splitters.split_into_train_test(data_subjects_allbut1, 3, 0)
+_, _, test_set = data_splitters.split_into_train_valid_test(data_subject_1, n_folds, 0)
 
 ################################################################################################################
 
