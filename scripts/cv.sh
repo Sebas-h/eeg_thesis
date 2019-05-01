4a4f38824d136b9ad611cfb7ecb5bdac16412510
Sebas <sebashigler@gmail.com>
2019-05-01 13:56:32 +0100

    adds

--- scripts/cv.sh
+++ scripts/cv.sh
@@ -1,9 +1,11 @@
 ##!/usr/bin/env bash
 #!/usr/bin/env zsh
 
-for i in 0 1 2 3 4 5 6 7 8
+#for i in 0 1 2 3 4 5 6 7 8
+for i in 0
 do
-    for j in 0 1 2 3 4
+#    for j in 0 1 2 3 4
+    for j in 0
     do
         sbatch /home/no316758/projects/eeg_thesis/scripts/args_pipeline.sh $i $j
     done
