068f97a9881d805380ea150f6f3d70d2a5950b0d
Sebas <sebashigler@gmail.com>
2019-06-11 15:17:52 +0100

    adds

--- scripts/test.py
+++ scripts/test.py
@@ -3,14 +3,16 @@ import os
 
 path = '/home/no316758/projects/eeg_thesis/scripts/args_run.sh'
 # path = '/Users/sebas/code/thesis/scripts/args_run.sh'
-subprocess.Popen(f'{path} {1} {0}', shell=True,
-                 stdin=subprocess.DEVNULL,
-                 stderr=subprocess.DEVNULL,
-                 stdout=subprocess.DEVNULL)
-subprocess.Popen(f'{path} {1} {1}', shell=True,
-                 stdin=subprocess.DEVNULL,
-                 stderr=subprocess.DEVNULL,
-                 stdout=subprocess.DEVNULL)
+subprocess.Popen(['/usr/local_rwth/bin/zsh', f'{path} {1} {0}'])
+
+# subprocess.Popen(f'{path} {1} {0}', shell=True,
+#                  stdin=subprocess.DEVNULL,
+#                  stderr=subprocess.DEVNULL,
+#                  stdout=subprocess.DEVNULL)
+# subprocess.Popen(f'{path} {1} {1}', shell=True,
+#                  stdin=subprocess.DEVNULL,
+#                  stderr=subprocess.DEVNULL,
+#                  stdout=subprocess.DEVNULL)
 
 # subprocess.Popen([f'{path} {1} {0}'])
 # subprocess.Popen([f'{path} {1} {1}'])
