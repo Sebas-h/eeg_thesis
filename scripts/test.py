4e4c9c11d0cea903b02e7438476060c0fb5e01e9
Sebas <sebashigler@gmail.com>
2019-06-11 14:59:12 +0100

    adds

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,15 @@
 import subprocess
 
 path = '/home/no316758/projects/eeg_thesis/scripts/args_run.sh'
+path = '/Users/sebas/code/thesis/scripts/args_run.sh'
 # subprocess.Popen([f'{path} {1} {0}'], shell=True)
 # subprocess.Popen([f'{path} {1} {1}'], shell=True)
-subprocess.Popen([f'{path} {1} {0}'])
-subprocess.Popen([f'{path} {1} {1}'])
+# subprocess.Popen([f'{path} {1} {0}'])
+# subprocess.Popen([f'{path} {1} {1}'])
+
+args = "#!/usr/bin/env zsh\nfor i in 1 2 3 4 5 6 7 8 9\ndo" \
+       "\nfor j in 0 1 2 3\ndo\nsbatch --job-name=JOB.${i}.${j} " \
+       "/home/no316758/projects/eeg_thesis/scripts/args_run.sh " \
+       "${i} ${j}\ndone\ndone"
+
+subprocess.Popen(args, shell=True)
