3957c621f49fbd36422cdb1dc02ead6b828baa19
Sebas <sebashigler@gmail.com>
2019-06-11 15:32:19 +0100

    adds

--- scripts/test.py
+++ scripts/test.py
@@ -2,27 +2,8 @@ import subprocess
 
 path = '/home/no316758/projects/eeg_thesis/scripts/args_run.sh'
 # path = '/Users/sebas/code/thesis/scripts/args_run.sh'
-args = "/home/no316758/projects/eeg_thesis/scripts/args_run.sh"
 
-# sbatch --job-name=JOB.${i}.${j} /home/no316758/projects/eeg_thesis/scripts/args_run.sh ${i} ${j}
-cmd = subprocess.Popen(f'sbatch --job-name=TESTJOB {path} {1} {0}', shell=True)
-# cmd.terminate()
+subprocess.Popen(f'sbatch --job-name=TESTJOB {path} {1} {0}', shell=True)
+subprocess.Popen(f'sbatch --job-name=TESTJOB {path} {1} {1}', shell=True)
+subprocess.Popen(f'sbatch --job-name=TESTJOB {path} {1} {2}', shell=True)
 
-# subprocess.Popen(f'{path} {1} {0}', shell=True,
-#                  stdin=subprocess.DEVNULL,
-#                  stderr=subprocess.DEVNULL,
-#                  stdout=subprocess.DEVNULL)
-# subprocess.Popen(f'{path} {1} {1}', shell=True,
-#                  stdin=subprocess.DEVNULL,
-#                  stderr=subprocess.DEVNULL,
-#                  stdout=subprocess.DEVNULL)
-
-# subprocess.Popen([f'{path} {1} {0}'])
-# subprocess.Popen([f'{path} {1} {1}'])
-
-# args = "#!/usr/bin/env zsh\nfor i in 1 2 3 4 5 6 7 8 9\ndo" \
-#        "\nfor j in 0 1 2 3\ndo\nsbatch --job-name=JOB.${i}.${j} " \
-#        "/home/no316758/projects/eeg_thesis/scripts/args_run.sh " \
-#        "${i} ${j}\ndone\ndone"
-#
-# subprocess.Popen(args, shell=True)
