044cafe94c7a95f2bc407a4d3bd764a7c852b5b8
Sebas <sebashigler@gmail.com>
2019-06-11 14:38:09 +0100

    adds

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,7 @@
 import subprocess
 
 path = '/home/no316758/projects/eeg_thesis/scripts/args_run.sh'
-subprocess.Popen([f'{path} {1} {0}'], shell=True)
-subprocess.Popen([f'{path} {1} {1}'], shell=True)
+# subprocess.Popen([f'{path} {1} {0}'], shell=True)
+# subprocess.Popen([f'{path} {1} {1}'], shell=True)
+subprocess.Popen([f'{path} {1} {0}'])
+subprocess.Popen([f'{path} {1} {1}'])
